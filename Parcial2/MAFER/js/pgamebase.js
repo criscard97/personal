0e2651c23a3a7310a19cc1eb2bdb2597f069ddfa
criscard97 <93285917+criscard97@users.noreply.github.com>
2024-11-17 23:03:53 -0600

    Update pgamebase.js

--- Parcial2/MAFER/js/pgamebase.js
+++ Parcial2/MAFER/js/pgamebase.js
@@ -756,7 +756,7 @@ const config = {
     }, physics: {
         default: 'arcade',
         arcade: {
-            debug: true,
+            debug: false,
             gravity: {
                 y: 300,
 
